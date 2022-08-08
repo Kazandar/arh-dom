4442f1643134347e5d8264eab32d6f37d16dd4d1
Долганин Игорь Александрович <igor_dolganin@epam.com>
2022-08-08 21:12:05 +0300

    adding my files to my repository

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const subMenuElem = $(".header_sub_menu");
+let isOpen = false;
+
+function scrollToPosition(position) {
+  $("body,html").animate({ scrollTop: position }, 500);
+}
+
+function ShowMenu() {
+  isOpen = true;
+  $(".menu img:first-child").hide();
+  $(".menu img:last-child").show();
+  subMenuElem.show();
+}
+
+function hideMenu() {
+  isOpen = false;
+  $(".menu img:last-child").hide();
+  $(".menu img:first-child").show();
+  subMenuElem.hide();
+}
+
+function onMenuClickHandler() {
+  isOpen ? hideMenu() : ShowMenu();
+}
+
+function onArrowClickHandler() {
+  const position = $("#our_services").offset().top;
+
+  scrollToPosition(position);
+}
+
+function onLinkClickHandler(event) {
+  event.preventDefault();
+
+  const id = $(this).attr("href");
+  const position = $(id).offset().top;
+
+  scrollToPosition(position);
+
+  hideMenu();
+}
+
+$(".header_nav_menu").on("click", "a", onLinkClickHandler);
+$(".header_sub_menu nav").on("click", "a", onLinkClickHandler);
+$(".arrow").on("click", onArrowClickHandler);
+$(".menu").on("click", onMenuClickHandler);
